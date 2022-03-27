47992b83855738fbb5feb44a7b672490c4050443
DimaMirk <dima.myruk@gmail.com>
2022-03-27 14:33:42 +0300

    add some logic

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -13,20 +13,42 @@ export class HomePageComponent implements OnInit {
 	@Input() curentCurrency: Observable<any> | undefined
 
 	currencyArr = []
-	сurrencyObj = {}
+	сurrencyValueObj = {}
+
+	inputValue: any = {}
+
+	valOneInt = ''
+	valTwoInt = ''
+
+	resultValInpTwo = '223'
 
 	ngOnInit(): void {
 		this.curentCurrency?.subscribe(data => {
 			this.currencyArr = data
+			this.createObjWithCurrencyValue()
 		})
 	}
 
-	onClick() {
+	createObjWithCurrencyValue() {
 		this.currencyArr.map(({ ccy, buy }) => {
-			this.сurrencyObj
 			const c = ccy
 			const v = buy
-			this.сurrencyObj[c] = v
+			this.сurrencyValueObj[c] = v
 		})
+	}
+	getInpValue = (event: Event) => {
+		const value = (event.target as HTMLInputElement).value
+		const key = (event.target as HTMLInputElement).id
+		this.inputValue[key] = value
+	}
+
+	onClick() {
+		console.log(this.сurrencyValueObj)
+		console.log(this.inputValue)
+		console.log(this.inputValue)
+		console.log(this.valTwoInt)
+		const r = 3 * 4
+
+		this.resultValInpTwo = r.toString()
 	}
 }
