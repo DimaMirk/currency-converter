efb5fb9b4655a8ad97c66354abc0989801ff772f
DimaMirk <dima.myruk@gmail.com>
2022-03-31 17:59:30 +0300

    deploy

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { Observable } from 'rxjs'
-import { ApiService } from '../services/api.service'
+
+import { CurentCurrencyService } from '../services/curent-currency.service'
+import { ConverterService } from '../services/converter.service'
+import { ICurShortInfo } from '../interfaces/ICurShortInfo'
 
 @Component({
 	selector: 'app-home-page',
@@ -8,47 +11,101 @@ import { ApiService } from '../services/api.service'
 	styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-	constructor(private service: ApiService) {}
+	constructor(
+		private currencyService: CurentCurrencyService,
+		private countService: ConverterService
+	) {}
 
 	@Input() curentCurrency: Observable<any> | undefined
 
-	currencyArr = []
-	сurrencyValueObj = {}
+	curentCurrencyObj: any = {}
 
-	inputValue: any = {}
+	selectOne = ''
+	selectTwo = ''
 
-	valOneInt = ''
-	valTwoInt = ''
-
-	resultValInpTwo = '223'
+	inptOneValue = ''
+	inputTwoValue = ''
 
 	ngOnInit(): void {
-		this.curentCurrency?.subscribe(data => {
-			this.currencyArr = data
-			this.createObjWithCurrencyValue()
-		})
+		this.curentCurrencyObj = this.currencyService.getCurrencyValueObj(this.curentCurrency)
+		this.countService.createCurrentCurrencyObj(this.curentCurrencyObj)
 	}
 
-	createObjWithCurrencyValue() {
-		this.currencyArr.map(({ ccy, buy }) => {
-			const c = ccy
-			const v = buy
-			this.сurrencyValueObj[c] = v
-		})
-	}
-	getInpValue = (event: Event) => {
-		const value = (event.target as HTMLInputElement).value
-		const key = (event.target as HTMLInputElement).id
-		this.inputValue[key] = value
+	onChangeSelect() {
+		if (this.selectOne === this.selectTwo) {
+			this.inputTwoValue = this.inptOneValue
+		}
+		if (this.inptOneValue !== '' && this.selectOne === 'UAH') {
+			this.countValue(+this.inptOneValue)
+			return
+		} else {
+			let result = this.countService.convertToUah(this.selectOne, +this.inptOneValue)
+			this.countValue(result)
+		}
 	}
 
-	onClick() {
-		console.log(this.сurrencyValueObj)
-		console.log(this.inputValue)
-		console.log(this.inputValue)
-		console.log(this.valTwoInt)
-		const r = 3 * 4
+	countValue(value: number) {
+		if (this.selectTwo === 'UAH') {
+			this.inputTwoValue = value.toFixed().toString()
+			return
+		} else {
+			const valueIpt = value
+			const nominalFromSelect = this.selectTwo
 
-		this.resultValInpTwo = r.toString()
+			const result = this.countService.countValue(nominalFromSelect, valueIpt)
+			this.inputTwoValue = result.toString()
+		}
+	}
+
+	getOneInpValue = (event: Event) => {
+		this.inptOneValue = (event.target as HTMLInputElement).value
+		if (this.selectOne === this.selectTwo) {
+			this.inputTwoValue = this.inptOneValue
+		}
+		this.checkCur(this.selectOne, +this.inptOneValue)
+	}
+
+	getTwoInpValue = (event: Event) => {
+		this.inputTwoValue = (event.target as HTMLInputElement).value
+		this.updateInpOne()
+	}
+
+	updateInpOne() {
+		if (this.selectTwo == this.selectOne) {
+			this.inptOneValue = this.inputTwoValue
+			return
+		}
+		if (this.selectOne === 'UAH') {
+			const changeValue = (
+				+this.inputTwoValue * this.curentCurrencyObj[`${this.selectTwo}`]
+			).toFixed(2)
+			this.inptOneValue = changeValue.toString()
+			return
+		} else {
+			const changeValueUah = (
+				+this.inputTwoValue * this.curentCurrencyObj[`${this.selectTwo}`]
+			).toFixed(2)
+			let convertResult = +changeValueUah / this.curentCurrencyObj[`${this.selectOne}`]
+			this.inptOneValue = convertResult.toFixed(2).toString()
+		}
+	}
+
+	checkCur(cur: string, value: number) {
+		if (cur === 'UAH') {
+			this.countValue(value)
+			return
+		} else {
+			let result = this.countService.convertToUah(cur, value)
+			this.countValue(result)
+			return
+		}
+	}
+	change() {
+		let newValueSelectTwo = this.selectOne
+		let newValueSelectOne = this.selectTwo
+		this.selectOne = newValueSelectOne
+		this.selectTwo = newValueSelectTwo
+
+		this.onChangeSelect()
 	}
 }
